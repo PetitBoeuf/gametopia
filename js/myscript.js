dbe443ac5cb017d7bd1375d6847c20b262c8d1bd
unknown <marcojl2000@hotmail.com>
2021-02-19 16:33:31 +0100

    Esthetically correct setting page, gotta run with kiki the kingos now

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,21 @@
-var centeredImage;
+var imageOnMouse;
 var fioc,sioc,tioc;
 var tempScrollAlt, tempScrollSrc;
 var games = ["langue", "prank", "kiki"], indexGame = 1, nbGames = 3; 
+var p1button, p2button;
 
 function AnimateMe(){
-    centeredImage = document.querySelector('#centeredOne');
-    let tempSrc = centeredImage.src;
-    centeredImage.src = centeredImage.alt;
-    centeredImage.alt = tempSrc;
+    imageOnMouse = document.querySelector('#centeredOne');
+    document.querySelector("#centeredOne") == null ? imageOnMouse = document.querySelector(".GameOverview") : imageOnMouse = document.querySelector("#centeredOne");
+    let tempSrc = imageOnMouse.src;
+    imageOnMouse.src = imageOnMouse.alt;
+    imageOnMouse.alt = tempSrc;
 }
 
 function DeanimateMe(){
-    let tempAlt = centeredImage.alt;
-    centeredImage.alt = centeredImage.src;
-    centeredImage.src = tempAlt;
+    let tempAlt = imageOnMouse.alt;
+    imageOnMouse.alt = imageOnMouse.src;
+    imageOnMouse.src = tempAlt;
 }
 
 function ScrollRight(){
@@ -44,6 +46,12 @@ function Play(){
     console.log("Vous allez donc jouer à : " + games[indexGame]);
 }
 
+function Select1P(){
+    
+    p1button = document.querySelectorAll(".GameMode")[0];
+    p2button = document.querySelectorAll(".GameMode")[1];
+    
+}
 /*
 1."https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif"
 2."https://media.giphy.com/media/JVGLHEuzbVviw/giphy.gif"
