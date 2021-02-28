09e14848adbb0191dd5ee7650ae6d70027d03bbc
Marco Jorda <marcojl2000@hotmail.com>
2021-02-28 13:31:53 +0100

    ending letsplay.html

--- js/background.js
+++ js/background.js
@@ -2,6 +2,8 @@
 var controllers = [];
 var startPositions = []
 var x = 500, y = 500; 
+var gameCanvas;
+var letsplay = false;
 
 function preload(){
     for(let indexController = 0; indexController < 10; indexController++){
@@ -14,17 +16,28 @@ function preload(){
 
 function setup(){
     bgCanvas = createCanvas(window.innerWidth,window.innerHeight);
-    bgCanvas.class('background');
+    if(document.querySelector("#GameCanvas")){
+        gameCanvas = createGraphics(500,600);
+        gameCanvas.parent("#GameCanvas");
+        letsplay = true;
+    }
+    //bgCanvas.class('background');
     //console.log(controllers);
 }
 
 function draw(){    
-    background(160,160,160);
-
+    background(160);
+    if (letsplay){
+        drawGameCanvas();
+    }
     /*
     for(let indexController = 0; indexController < 10; indexController++){
         image(controllers[indexController], x+=random(-10,10), y+=random(-10,10),175,150);  
     } 
     */   
 
+}
+
+function drawGameCanvas(){
+    gameCanvas.background(0,0,0);
 }
